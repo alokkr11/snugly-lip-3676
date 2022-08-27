b1dafa44b5709de25c70891e39e9eec7d97afaf9
sachin9603 <sachinparmar3803@gmail.com>
2022-08-27 17:17:36 +0530

    allfiles

---
+++ scripts/services1.js
@@ -0,0 +1,26 @@
+// let cat = document.addEventListner("click",()=>{
+// getdata()
+// })
+//  let dog = document.getElementById("box1_threediv_firstdiv").addEventListener("click", ()=>{
+//     getdata()
+//  })
+//  let cat = document.getElementById("box1_threediv_secdiv").addEventListener("click", ()=>{
+//     getdata()
+//  })
+ 
+ 
+//  let mon = document.getElementById("box1_threediv_thirddiv").addEventListener("click", ()=>{
+//     getdata()
+//  })
+ 
+// let getdata = ()=>{ 
+//     alert("i am clicked ")
+//     window.location.herf="services2.html"
+// }
+// let knowmore = document.getElementsByClassName("knmore").addEventListener("click", ()=>{
+//         getdata()
+//      })
+//      let getdata = ()=>{
+//         alert("i am clicked ")
+//     window.location.herf="service2"
+// }
